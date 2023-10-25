94f42f7e8bdcc8e8c9c954bb50bb887a3608a2c1
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-25 15:38:19 +0300

    Update Music.java

--- src/Hobby/Music.java
+++ src/Hobby/Music.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Hobby {
+package Hobby;
+
+public class Music {
+    public String band;
+    public int albums;
+    public String style;
+    public int members;
+    private String studio;
+
+    public void setAlbums(int albums) {
+        this.albums = albums;
+    }
+    public void setBand(String band) {
+        this.band = band;
+    }
+    public void setMembers(int members) {
+        this.members = members;
+    }
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+    public void setStyle(String style) {
+        this.style = style;
+    }
+    public String getStudio() {
+        return studio;
+    }
+    public int getAlbums() {
+        return albums;
+    }
+    public int getMembers() {
+        return members;
+    }
+    public String getBand() {
+        return band;
+    }
+    public String getStyle() {
+        return style;
+    }
+    public Music(){
+        this.band = "Band";
+        this.style = "Style";
+        this.studio = "Studio";
+        this.members = 4;
+        this.albums = 5;
+    }
 }
+
