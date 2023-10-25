f3ab0d1347509a7e5eba00b83c8277cb67ccda93
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-25 15:38:41 +0300

    Update Horse.java

--- src/Horse.java
+++ src/Horse.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Horse {
+public class Horse {
+    String name;
+    String color;
+    int age;
+
+    public Horse(String name, String color, int age){
+        this.name= name;
+        this.color = color;
+        this.age = age;
+        System.out.println("Name: "+name+ " Color: "+ "Age: " +age);
+    }
 }
+
