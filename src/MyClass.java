b8ef88aadc315c367ad92a6d541669632717b8d7
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-25 15:40:10 +0300

    Update MyClass.java

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class MyClass {
+public class MyClass {
+
+    private String course;
+
+    private String getCourse() {
+        return course;
+    }
+
+    private void setCourse(String course) {
+        this.course = course;
+    }
+    private static void forError(String s){
+        System.out.println("Error");
+    }
 }
