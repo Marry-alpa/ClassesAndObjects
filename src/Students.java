b47e4736c2081d16ff6213c77c755ca6ad69895c
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-25 15:40:44 +0300

    Update Students.java

--- src/Students.java
+++ src/Students.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Students {
+public class Students {
+    String name;
+    String surname;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
