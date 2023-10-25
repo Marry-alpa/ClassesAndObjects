9a201e8ccf299542ad1ccf6e745aa7d9f7ac3197
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-25 15:39:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyClass obj = new MyClass();
+        MyClassWithMethod obj2 = new MyClassWithMethod();
+        //obj.forError("Error");
+        obj2.print("Print something");
+        Horse horse = new Horse("Bruno", "orange", 7);
+        Students students = new Students();
+
+        students.setName("Katrin");
+        students.setSurname("Madrigal");
+        students.setAge(18);
+
+        System.out.printf(
+                "Name: %s Surname: %s Age: %d",
+                students.getName(),
+                students.getSurname(),
+                students.getAge());
+        //obj.setCourse("Course");
     }
 }
